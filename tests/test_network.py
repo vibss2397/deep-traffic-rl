e7575671ff9ade2e40341f1c821426d935753f6d
vibhu bhatia <vibhu2397@gmail.com>
2018-02-28 00:24:07 +0530

    sdfsdfsf

--- tests/test_network.py
+++ tests/test_network.py
@@ -5,19 +5,19 @@ import numpy as np
 
 
 arr1=np.random.rand(1,128)
-arr2=np.random.rand(1,128)
+arr2=arr1*0.92
 
 def calc_dist(a,b):
     dist=np.sum(np.square(a-b))
     dist_sqroot=np.sqrt(max(dist,K.epsilon()))
-    return dist_sqroot.astype(np.float32)
+    return dist_sqroot
 
 def test_eucledian_distance():
     image0 = K.variable(value=arr1)
     image1 = K.variable(value=arr2)
     dist = network.euclidean_distance([image0,image1])
     evaluate=K.eval(dist)
-    assert (evaluate==calc_dist(arr1,arr2))
+    assert (evaluate==calc_dist(arr1,arr2).astype(np.float32))
 
 def test_eucl_distance_output_shape():
     vect=[arr1.shape,arr2.shape]
@@ -31,7 +31,11 @@ def test_contrastive_loss():
     loss2=network.contrastive_loss(0,euclid_dist) #if we assume 0 was the output
     eval1=K.eval(loss1)
     eval2=K.eval(loss2)
-    assert(eval1>=0)
-    assert(eval2>=0)
+    assert (eval1>=0)
+    assert (eval2>=0)
 
-# def test_
+def test_accuracy():
+    euclid_dist = calc_dist(arr1, arr2)
+    loss1 = network.accuracy(1, euclid_dist)  # if we assume 1 was the output
+    eval1 = K.eval(loss1)
+    print (eval1 >= 0)
